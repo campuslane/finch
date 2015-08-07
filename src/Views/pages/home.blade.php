da7392f372833381fdccf561639bc8765cd0e2ce
frankmatull <frankm@campuslane.com>
2015-08-07 09:50:08 -0700

    added page route and home page view

---
+++ src/Views/pages/home.blade.php
@@ -0,0 +1,10 @@
+@extends('finch::layouts.page')
+
+@section('title', 'Home Page')
+
+@section('content')
+
+    <h1>My Home Page</h1>
+    <p>This is the front end home page found in Finch views...</p>
+
+@endsection
--- src/routes.php
+++ src/routes.php
@@ -4,3 +4,13 @@
 Route::get('finch', function(){
     return view('finch::welcome');
 });
+
+// page route
+Route::get('{slug}', function($slug){
+
+    $view = "finch::pages.$slug";
+
+    if (view()->exists($view)) {
+        return view($view);
+    }
+});
